f9b522bc416788c995bfa30c281cd56e8bb902e4
Mani <12841290+krmanik@users.noreply.github.com>
2023-01-30 00:20:54 +0530

    ui freeze fix

--- src/batch_edit.py
+++ src/batch_edit.py
@@ -16,72 +16,30 @@ from anki.hooks import addHook
 
 from .editor import getRandomSentence, config_data
 
+from datetime import datetime
+
 folder = os.path.dirname(__file__)
 
-class SentenceBatchEdit(QDialog):
-    def __init__(self, browser, nids):
-        QDialog.__init__(self, parent=browser)
-        self.setWindowTitle("Sentence Batch Adder")
-        self.browser = browser
+
+class GetSentenceThread(QThread):
+    finish = pyqtSignal(int)
+
+    def __init__(self, mw1, nids, wordField, senField):
+        QThread.__init__(self)
+        self.mw1 = mw1
         self.nids = nids
+        self.wordField = wordField
+        self.senField = senField
 
-        self.resize(400, 300)
+    def run(self):
+        count = 0
+        tstamp = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+        out = open(folder + "/user_files/not_found_" + tstamp + ".txt", "w", encoding="utf-8")
 
-        layout = QVBoxLayout()
-
-        topLayout = QFormLayout()
-
-        self.selectFieldsComboBox = QComboBox()
-        self.wordsComboBox = QComboBox()
-
-        nid = self.nids[0]
-        self.mw = self.browser.mw
-        note_type = self.mw.col.get_note(nid).note_type()
-        fields = self.mw.col.models.field_names(note_type)
-        
-        self.selectFieldsComboBox.addItems(fields)
-        self.selectFieldsComboBox.setCurrentText(fields[0])
-
-        self.wordsComboBox.addItems(fields)
-        self.wordsComboBox.setCurrentText(fields[0])
-
-        self.auto_add_rb = QRadioButton("Auto Add")
-        self.all_sen_win_rb = QRadioButton("Open All Sentences Window")
-
-
-        topLayout.addRow(QLabel("<b>Select fields and start batch add</b>"))
-
-        topLayout.addRow(QLabel("Select words field"), self.wordsComboBox)
-        topLayout.addRow(QLabel("Select sentence field"), self.selectFieldsComboBox)
-        
-        # topLayout.addRow(self.auto_add_rb)
-        # topLayout.addRow(self.all_sen_win_rb)
-
-        buttonBoxLayout = QHBoxLayout()
-
-        self.buttonBox = QDialogButtonBox()
-        self.buttonBox.addButton("Start", QDialogButtonBox.ButtonRole.AcceptRole)
-        self.buttonBox.addButton("Close", QDialogButtonBox.ButtonRole.RejectRole)
-
-        self.buttonBox.accepted.connect(self.startBatchAdder)
-        self.buttonBox.rejected.connect(self.close)
-
-        buttonBoxLayout.addWidget(self.buttonBox)
-
-        layout.addLayout(topLayout)
-        layout.addLayout(buttonBoxLayout)
-        self.setLayout(layout)
-
-    def startBatchAdder(self):
-        wordField = self.wordsComboBox.currentText()
-        senField = self.selectFieldsComboBox.currentText()
-
-        out = open(folder + "/user_files/not_found.txt", "w", encoding="utf-8")
-        
         for nid in self.nids:
-            note = self.mw.col.get_note(nid)
-            if wordField in note:
-                word = note[wordField]
+            note = self.mw1.col.get_note(nid)
+            if self.wordField in note:
+                word = note[self.wordField]
                 randomSen = getRandomSentence(word)
                 print(randomSen)
 
@@ -109,14 +67,94 @@ class SentenceBatchEdit(QDialog):
                             sen = sen_html[0] + sen + sen_html[1]
 
                         if config_data['text_color']:
-                            note[senField] += '<font color="' + config_data['text_color'] + '">' + sen + "</font>"
+                            note[self.senField] += '<font color="' + config_data['text_color'] + '">' + sen + "</font>"
                         else:
-                            note[senField] += sen
+                            note[self.senField] += sen
+
+                        note[self.senField] += "<br>"
+                    count += 1
                 else:
                     tooltip("Sentence not found for " + word)
-                    out.write(word+"\n")
+                    out.write(word + "\n")
                 note.flush()
-            
+        self.finish.emit(count)
+
+
+class SentenceBatchEdit(QDialog):
+    def __init__(self, browser, nids):
+        QDialog.__init__(self, parent=browser)
+        self.setWindowTitle("Sentence Batch Adder")
+        self.browser = browser
+        self.nids = nids
+
+        self.resize(400, 300)
+
+        layout = QVBoxLayout()
+
+        topLayout = QFormLayout()
+
+        self.selectFieldsComboBox = QComboBox()
+        self.wordsComboBox = QComboBox()
+
+        nid = self.nids[0]
+        self.mw = self.browser.mw
+        note_type = self.mw.col.get_note(nid).note_type()
+        fields = self.mw.col.models.field_names(note_type)
+
+        self.selectFieldsComboBox.addItems(fields)
+        self.selectFieldsComboBox.setCurrentText(fields[0])
+
+        self.wordsComboBox.addItems(fields)
+        self.wordsComboBox.setCurrentText(fields[0])
+
+        self.auto_add_rb = QRadioButton("Auto Add")
+        self.all_sen_win_rb = QRadioButton("Open All Sentences Window")
+
+
+        topLayout.addRow(QLabel("<b>Select fields and start batch add</b>"))
+
+        topLayout.addRow(QLabel("Select words field"), self.wordsComboBox)
+        topLayout.addRow(QLabel("Select sentence field"), self.selectFieldsComboBox)
+
+        # topLayout.addRow(self.auto_add_rb)
+        # topLayout.addRow(self.all_sen_win_rb)
+
+        buttonBoxLayout = QHBoxLayout()
+
+        self.buttonBox = QDialogButtonBox()
+        self.buttonBox.addButton("Start", QDialogButtonBox.ButtonRole.AcceptRole)
+        self.buttonBox.addButton("Close", QDialogButtonBox.ButtonRole.RejectRole)
+
+        self.buttonBox.accepted.connect(self.startBatchAdder)
+        self.buttonBox.rejected.connect(self.close)
+
+        buttonBoxLayout.addWidget(self.buttonBox)
+
+        layout.addLayout(topLayout)
+        layout.addLayout(buttonBoxLayout)
+        self.setLayout(layout)
+        self.get_sen_thread = None
+
+    def startBatchAdder(self):
+        mw = self.browser.mw
+        mw.checkpoint("batch edit")
+        mw.progress.start()
+        self.browser.model.beginReset()
+
+        wordField = self.wordsComboBox.currentText()
+        senField = self.selectFieldsComboBox.currentText()
+        self.get_sen_thread = GetSentenceThread(self.browser.mw, self.nids, wordField, senField)
+        self.get_sen_thread.finish.connect(self.finished)
+        self.get_sen_thread.start()
+
+    def finished(self, result):
+        mw = self.browser.mw
+        self.browser.model.endReset()
+        mw.progress.finish()
+        mw.reset()
+        tooltip("<b>Updated</b> {0} notes.".format(result), parent=self.browser)
+
+
 def onSentenceBatchEdit(browser):
     nids = browser.selectedNotes()
     if not nids:
